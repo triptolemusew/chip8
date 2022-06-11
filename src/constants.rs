d39aacc81dec42f36f872446d3b8ab1e16393419
faris <deneuve.crepes@gmail.com>
2022-06-12 00:26:25 +0800

    Fix proper screen height & width

--- src/constants.rs
+++ src/constants.rs
@@ -1,7 +1,4 @@
-pub const SCREEN_HEIGHT: usize = 64;
-pub const SCREEN_WIDTH: usize = 32;
-
-pub const MEMORY_SIZE: u32 = 0x4096;
-pub const MEMORY_OFFSET: u32 = 0x200;
+pub const SCREEN_HEIGHT: usize = 32;
+pub const SCREEN_WIDTH: usize = 64;
 
 pub const PROGRAM_START: u16 = 0x200;
