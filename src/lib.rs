490e7bce4624d313776c0bf8394ee42f9ac2d9ac
faris <deneuve.crepes@gmail.com>
2022-06-16 23:02:25 +0800

    Add boilerplate driver

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,59 @@
-// mod wasm;
+extern crate js_sys;
+extern crate wasm_bindgen;
+extern crate web_sys;
+
+use js_sys::Math;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+mod bus;
+mod cpu;
+mod rom;
+mod constants;
+mod memory;
+mod display;
 
 #[wasm_bindgen]
 extern "C" {
-    fn alert(s: &str);
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+
+    #[wasm_bindgen(js_namespace = console)]
+    fn error(s: &str);
+}
+
+fn window() -> web_sys::Window {
+    web_sys::window().expect("global `window` should be OK.")
+}
+
+fn request_animation_frame(f: &Closure<dyn FnMut()>) {
+    window()
+        .request_animation_frame(f.as_ref().unchecked_ref())
+        .expect("`requestAnimationFrame` should be OK.");
+}
+
+fn cancel_animation_frame(id: i32) {
+    window()
+        .cancel_animation_frame(id)
+        .expect("`cancelAnimationFrame` should be OK.");
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}", name));
+pub fn run() -> Result<(), JsValue> {
+    let document = window().document().expect("window should have a document");
+    let body = document.body().expect("document should have a body");
+
+    let f = Rc::new(RefCell::new(None));
+    let g = f.clone();
+
+    let mut i = 0;
+    *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
+        // TODO: The main logic will be here.
+    }) as Box<dyn FnMut()>));
+
+    request_animation_frame(g.borrow().as_ref().unwrap());
+    Ok(())
 }
